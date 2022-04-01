f22201707616b6315a249068f3c1cee35fed7516
Fanaar Rashid <fanaarrashid@gmail.com>
2022-04-01 13:38:53 +0200

    Fixed roof. Hid cursor.

---
+++ Assets/Scripts/Hidden_Cursor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hidden_Cursor : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;  
+    }
+
+
+}
